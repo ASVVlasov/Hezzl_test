bd51cc6ef48ded201f322bfd2b9d322928991504
ASVVlasov <asvvlasov@gmail.com>
2020-04-16 13:06:28 +0300

    fix(project): fix router and build for GitHubPages

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import {RegistrationComponent} from './modules/auth/components/registration/registration.component';
-import {AccountPageComponent} from './modules/account/components/account-page/account-page.component';
 import {AuthGuard} from './services/guards/auth.guard';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
   {
     path: 'auth',
     children: [
@@ -29,7 +32,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
