79d0d79d71432eea4487930529418eb4cf60946f
ASVVlasov <asvvlasov@gmail.com>
2020-04-16 12:43:48 +0300

    feat(auth.service): registration from auth.service

--- app/src/app/services/auth.service.ts
+++ app/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {UserModel} from '../models/user.model';
 import {HttpClient} from '@angular/common/http';
 import {map, tap} from 'rxjs/operators';
@@ -21,8 +21,18 @@ export class AuthService {
     return this.currentUserSubject.value;
   }
 
-  login(userName: string, password: string) {
+  login(userName: string, password: string): Observable<any> {
     return this.httpClient.post(`http://localhost:4200/auth/login`, { userName, password })
+      .pipe(
+        tap((user: UserModel) => {
+          localStorage.setItem('User', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        })
+      );
+  }
+
+  registration(body: {any}): Observable<any> {
+    return this.httpClient.post(`http://localhost:4200/auth/registration`, body)
       .pipe(
         tap((user: UserModel) => {
           localStorage.setItem('User', JSON.stringify(user));
